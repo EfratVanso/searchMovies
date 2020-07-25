3ce277a9581a94133e3ccf3a8dc91c430c1b6911
efiVanso <efi.vanso@gmail.com>
2020-07-26 01:19:40 +0300

    add omdbapi api
    http://www.omdbapi.com/

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ const express = require('express');
 const app = express();
 app.set("view engine", "ejs");
 var axios = require('axios').default;
+//var pry = require('pryjs');
+console.log('=======================================================================');
+
+
+app.get("/results", function(req,res){
+	
+	axios.get('http://www.omdbapi.com/?s=stars&apikey=thewdb')
+    .then(function (response) {
+        //eval(pry.it)
+        console.log(response.data);
+		var x = response.data;
+		res.send( response.data.Search[0]);
+		
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    });
+		
+
+});
 
 
 
